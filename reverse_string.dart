b0a24995c9b62224000415188918c86fdb11252d
Patel Rudrakumar <hitendra22051914@gmail.com>
2025-10-31 11:53:35 +0530

    reverse_string

---
+++ reverse_string.dart
@@ -0,0 +1,19 @@
+void myswap(List<String>a,int i,int j){
+  String t=a[i];
+  a[i]=a[j];
+  a[j]=t;
+}
+
+void main(){
+  String s="rudra";
+  List<String> a=s.split("");
+  int l=0, r=a.length-1;
+
+  while(l<r){
+    myswap(a,l,r);
+    l++;
+    r--;
+  }
+
+  print(a.join());
+}
